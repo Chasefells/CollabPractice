473d6200a0310d7b3b87fc73d990628360fa61d9
Your Name <your@email.com>
2026-02-19 20:20:14 -0800

    Added print message Here are the project team members: with names

--- ChaseProgram.java
+++ ChaseProgram.java
@@ -2,6 +2,7 @@
 //Practice
 public class ChaseProgram {
     public static void main(String[] args) {
+        System.out.println("Here are the project team members:Bryce, Danish, Asim, Leif, Chase");
         System.out.println("Hello from Chase's Java file!");
        
         System.out.println("Ready for merge conflicts practice.");
