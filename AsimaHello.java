7345f996af37f66ba3f62046d5c80bb4f7549ad7
asimrafiq002 <153468540+asimrafiq002@users.noreply.github.com>
2026-02-22 14:18:05 -0800

    Non-conflicting change #1: add output line (Asima)

--- AsimaHello.java
+++ AsimaHello.java
@@ -1,5 +1,6 @@
 public class AsimaHello {
     public static void main(String[] args) {
         System.out.println("Hello from CollabPractice!");
+        System.out.println("Asima: non-conflict 1");
     }
 }
