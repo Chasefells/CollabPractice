4138f751e91a2df732448775f753b5ae1ee170da
asimrafiq002 <153468540+asimrafiq002@users.noreply.github.com>
2026-02-23 12:34:34 -0800

    Conflicting change #1 (local side): change agreed line (Asima)

--- AsimaHello.java
+++ AsimaHello.java
@@ -1,6 +1,6 @@
 public class AsimaHello {
     public static void main(String[] args) {
-        System.out.println("Hello from CollabPractice!");
+        System.out.println("My Name is Asim Rafiq - ASIMA VERSION");
         System.out.println("Asima: non-conflict 1");
         System.out.println("Asima: non-conflict 2");
         System.out.println("Asima: Non-conflict merge example");
