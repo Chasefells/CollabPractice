1942ec36ab1e789ad5a31f3ac8165a7a8f3ce2ff
asimrafiq002 <153468540+asimrafiq002@users.noreply.github.com>
2026-02-23 12:39:56 -0800

    Conflicting change #2 (origin/dev side): change agreed line

--- AsimaHello.java
+++ AsimaHello.java
@@ -1,13 +1,9 @@
 public class AsimaHello {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("My Name is Asim Rafiq - ASIMA VERSION");
-=======
-        System.out.println("My Name is Asim Rafiq - ORIGIN VERSION");
->>>>>>> origin/dev
         System.out.println("Asima: non-conflict 1");
         System.out.println("Asima: non-conflict 2");
-        System.out.println("Asima: non-conflict 3");
+        System.out.println("Conflict 2 - ORIGIN version");
         System.out.println("Asima: Non-conflict merge example");
     }
 }
